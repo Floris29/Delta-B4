92a89373814f4bbf6859607a5ab371875b6209c8
Floris <Florismelchers@gmail.com>
2024-05-27 12:35:12 +0200

    index creation

--- index.php
+++ index.php
@@ -6,9 +6,16 @@ require 'imports/header.php';
 
 <body>
     
-    <h1>Welkom bij Attractie AttractieManagement</h1>
+    <h1>Welkom bij Attractie Managment</h1>
+    <h4>Een webstie gebouwd voor de managers</h4>
 
+    <img src="" alt="Attractie manager logo">
 
+    <p>Attractie Managment is een website waar je als manager van een attractiepark je attracties kan beheren. 
+        Je kan hier nieuwe attracties toevoegen, bestaande attracties aanpassen en attracties verwijderen. 
+        Ook kan je hier de bezoekersaantallen van je attracties bekijken en de gemiddelde bezoekersaantallen van je attracties.</p>
+
+    <p>Als normale gebruiker kan je de gemiddelde wachttijden zien van elke attractie</p>
 
     
 </body>
