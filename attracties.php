5e2a1a2d007b82241b6374a91a33ca391cc63319
Renssus <RensWinckens@gmail.com>
2024-05-27 12:41:23 +0200

    css fix + Grid on attracties.php

--- attracties.php
+++ attracties.php
@@ -10,7 +10,68 @@ require 'imports/header.php';
 
 <body>
     
-    <img src="" alt="">
+
+    <div class="GridImagesAttracties">
+        <div class="Attractie">
+            <img src="assets/img/baron.png" alt="baron">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/bronzy.png" alt="bronzy">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/draaier.png" alt="draaier">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/duocoaster.png" alt="duocoaster">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        <div class="Attractie">
+            <img src="assets/img/Joris.png" alt="joris">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/masterofSpeed.png" alt="masterofSpeed">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <class="Attractie">
+            <img src="assets/img/nogravity.png" alt="nogravity">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/phoenix.png" alt="phoenix">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/piranja.png" alt="piranja">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/python.png" alt="python">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/reuzenrad.png" alt="reuzenrad">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+        <div class="Attractie">
+            <img src="assets/img/spinner.png" alt="spinner">
+            <p><strong>Baron</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
+    </div>
 </body>
 
 <?php
