3dbc2fa99e34b09c4bd361d3ad7d7d091d74f554
Renssus <RensWinckens@gmail.com>
2024-05-27 13:50:38 +0200

    CSS + Attracties.php

--- attracties.php
+++ attracties.php
@@ -16,21 +16,27 @@ require 'imports/header.php';
             <img src="assets/img/baron.png" alt="baron">
             <p><strong>Baron</strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+            <p>status: $status Wachttijd: $wachttijd</p>
         </div>
         <div class="Attractie">
             <img src="assets/img/bronzy.png" alt="bronzy">
             <p><strong>Baron</strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+            <p>status: $status</p>
+            <p>Wachttijd: $wachttijd</p>
         </div>
         <div class="Attractie">
             <img src="assets/img/draaier.png" alt="draaier">
             <p><strong>Baron</strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+            <p>status: $status</p>
+            <p>Wachttijd: $wachttijd</p>
         </div>
         <div class="Attractie">
             <img src="assets/img/duocoaster.png" alt="duocoaster">
             <p><strong>Baron</strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
+        </div>
         <div class="Attractie">
             <img src="assets/img/Joris.png" alt="joris">
             <p><strong>Baron</strong></p>
@@ -41,7 +47,7 @@ require 'imports/header.php';
             <p><strong>Baron</strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
         </div>
-        <class="Attractie">
+        <div class="Attractie">
             <img src="assets/img/nogravity.png" alt="nogravity">
             <p><strong>Baron</strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque, sed quis, veritatis corporis culpa error reprehenderit repellendus illo similique velit nisi, sunt atque commodi veniam incidunt minima animi totam.</p>
