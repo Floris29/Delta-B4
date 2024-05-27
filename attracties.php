2bbc9713f65093fba1c51428ff285bc826ed868a
Floris <Florismelchers@gmail.com>
2024-05-27 12:14:46 +0200

    pagina inrichting

---
+++ attracties.php
@@ -0,0 +1,12 @@
+<?php
+require 'imports/head.php';
+
+require 'imports/header.php';
+?>
+
+<body>
+    
+</body>
+
+<?php
+require 'imports/footer.php';
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AttractieManagement</title>
-</head>
+<?php
+require 'imports/head.php';
+
+require 'imports/header.php';
+?>
+
 <body>
     
-
-    
 </body>
-</html>
+
+<?php
+require 'imports/footer.php';
