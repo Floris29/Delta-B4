1b872fa84b0584003ce021f6d9298be1b5912821
Floris <Florismelchers@gmail.com>
2024-05-27 16:22:26 +0200

    kleine aanpassing aan de header

--- imports/header.php
+++ imports/header.php
@@ -5,6 +5,7 @@
 
     <?php if (isset($_SESSION['user_id'])): ?>
                 <a href="logout.php" class="nav-item">Logout</a>
+                <a href="adminpanel.php">AdminPanel</a>
             <?php else: ?>
                 <a href="login.php" class="nav-item">Login</a>
                 <a href="register.php" class="nav-item">Regristreren</a>
