eb1bfc1715919b7a49f8a80116fca6fdb5f98889
Floris <Florismelchers@gmail.com>
2024-05-27 12:42:19 +0200

    created register, and registerhandler file

--- register.php
+++ register.php
@@ -6,8 +6,31 @@ require 'imports/head.php';
 
 <?php
 require 'imports/header.php';
- ?>
-    
+?>
+
+        <h2>Registreer je voor de manager omgeving</h2>
+        
+        <form action="backend/registerHandler.php" method="POST">
+            <div class="form-group">
+                <label for="email">email:</label>
+                <input type="text" name="email">
+            </div>
+            <div class="form-group">
+                <label for="username">username:</label>
+                <input type="text" name="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password">
+            </div>
+            <div class="form-group">
+                <label for="password">Password check:</label>
+                <input type="password" name="password_check">
+            </div>
+            <input type="submit" value="Regristreren">
+        </form>
+
+
 </body>
 
 <?php
